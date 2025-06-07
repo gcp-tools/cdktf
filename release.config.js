2818ebecd82ff55e3699fab1e0f9a10c23b0acc2
Simon Jefford <simon.jefford@nimbla.com>
2025-06-07 15:41:15 +0100

    feat: adds publisher

---
+++ release.config.js
@@ -0,0 +1,79 @@
+/**
+ * @type {import('semantic-release').GlobalConfig}
+ */
+export default {
+  branches: [
+    'main',
+    {
+      name: 'alpha',
+      prerelease: true,
+    },
+  ],
+  repositoryUrl: 'https://github.com/gcp-tools/cdktf.git',
+  plugins: [
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        preset: 'conventionalcommits',
+        releaseRules: [
+          { type: 'feat', release: 'minor' },
+          { type: 'fix', release: 'patch' },
+          { type: 'perf', release: 'patch' },
+          { type: 'docs', release: 'patch' },
+          { type: 'refactor', release: 'patch' },
+          { type: 'style', release: 'patch' },
+          { type: 'build', release: 'patch' },
+          { type: 'ci', release: 'patch' },
+          { type: 'chore', release: 'patch' },
+          { type: 'revert', release: 'patch' },
+          { type: 'test', release: 'patch' },
+          { type: 'BREAKING CHANGE', release: 'major' },
+        ],
+      },
+    ],
+    [
+      '@semantic-release/release-notes-generator',
+      {
+        preset: 'conventionalcommits',
+        presetConfig: {
+          types: [
+            { type: 'feat', section: 'Features' },
+            { type: 'fix', section: 'Bug Fixes' },
+            { type: 'perf', section: 'Performance Improvements' },
+            { type: 'revert', section: 'Reverts' },
+            { type: 'docs', section: 'Documentation' },
+            { type: 'style', section: 'Styles' },
+            { type: 'refactor', section: 'Code Refactoring' },
+            { type: 'test', section: 'Tests' },
+            { type: 'build', section: 'Build System' },
+            { type: 'ci', section: 'Continuous Integration' },
+            { type: 'chore', section: 'Chores' },
+          ],
+        },
+      },
+    ],
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: 'CHANGELOG.md',
+        changelogTitle: '# Changelog',
+      },
+    ],
+    [
+      '@semantic-release/exec',
+      {
+        prepareCmd: 'npm run build',
+      },
+    ],
+    '@semantic-release/npm',
+    '@semantic-release/github',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['package.json', 'package-lock.json', 'CHANGELOG.md'],
+        message:
+          'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+  ],
+};
